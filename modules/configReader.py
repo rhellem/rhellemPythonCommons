7149c17c74a3140c15b0c0a741884ea6dadbe38f
Rune Hellem <rune.hellem@gmail.com>
2024-12-27 20:02:05 +0100

    Rebasing, since I do not know what I'm doing, but since I'm the only one doing it, I go ahead and add the module once again, but now from the repo, not the submodule

--- modules/configReader.py
+++ modules/configReader.py
@@ -12,8 +12,18 @@ class ConfigReader:
         self.config_path = config_path
         self.config_data = self._load_config()
         self._mask_passwords()  # Automatically mask passwords on initialization
+
+    def _load_config(self):
+        """
+        Loads the YAML configuration file.
         
-        
+        :return: Parsed YAML data as a dictionary.
+        """
+        if not os.path.exists(self.config_path):
+            raise FileNotFoundError(f"Configuration file not found: {self.config_path}")
+        with open(self.config_path, 'r') as file:
+            return yaml.safe_load(file)
+
     def _mask_passwords(self):
         """
         Masks the password fields in the loaded configuration data by replacing their
@@ -22,36 +32,6 @@ class ConfigReader:
         for db, values in self.config_data.items():
             if "password" in values:
                 values["password"] = "****"
-                
-    def __repr__(self):
-        """
-        Custom string representation of the object, showing the masked configuration data.
-        """
-        return f"ConfigReader({self.config_data})"
-    
-    def _load_config(self):
-        """
-        Loads the YAML configuration file.
-        
-        :return: Parsed YAML data as a dictionary.
-        # Assume the YAML config file is named "db_config.yaml" and looks like this:
-        my_database:
-             host: localhost
-             user: admin
-             password: secret
-             name: my_database
-         other_database:
-             host: 192.168.1.10
-             user: user2
-             password: password2
-             name: other_db
-
-        """
-        if not os.path.exists(self.config_path):
-            raise FileNotFoundError(f"Configuration file not found: {self.config_path}")
-
-        with open(self.config_path, 'r') as file:
-            return yaml.safe_load(file)
 
     def get_database_config(self, db_name):
         """
@@ -62,6 +42,10 @@ class ConfigReader:
         """
         if db_name not in self.config_data:
             raise ValueError(f"Database configuration for '{db_name}' not found in the config file.")
-
         return self.config_data[db_name]
 
+    def __repr__(self):
+        """
+        Custom string representation of the object, showing the masked configuration data.
+        """
+        return f"ConfigReader({self.config_data})"
